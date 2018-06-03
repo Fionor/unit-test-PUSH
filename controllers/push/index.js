db3099b944c4c978fd534fda7427fd9fa4a82bfa
Dmitry <kdp1999@yandex.ru>
2018-06-03 14:55:16 +0300

    add log message

--- controllers/push/index.js
+++ controllers/push/index.js
@@ -3,6 +3,7 @@ module.exports.send = (req, res) => {
         if(!req.body.room || req.body.room == '') return res.send({status: 400, errors: [{error_msg: 'invalid room id'}]});
         if(!req.body.statistic || req.body.statistic == '') return res.send({status: 400, errors: [{error_msg: 'invalid statistic'}]});
         res.io.to(req.body.room).emit('update_statistic', {statistic: req.body.statistic});
+        console.log(`SEND ${JSON.stringify(req.body.statistic)} INTO ${req.body.room}`);
         res.send({status: 200});
     } catch (error) {
         console.log('push error', error);
